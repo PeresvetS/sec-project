bb6f824a047cd0a742b917d5cda602dca26ab00f
PeresvetS <niks280593@gmail.com>
2017-10-19 19:16:49 +0300

    delete db

--- src/init.js
+++ src/init.js
@@ -1,8 +1,8 @@
-import connect from './db';
-import getModels from './models';
+// import connect from './db';
+// import getModels from './models';
 
-export default async () => {
-  const models = getModels(connect);
+// export default async () => {
+//   const models = getModels(connect);
 
-  await models.Default.sync({ force: true });
-};
+//   await models.Default.sync({ force: true });
+// };
